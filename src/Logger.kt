727546018a45f45d7626e2bd7dab6058167bd801
INAH-PAK <dinho3@gmail.com>
2022-11-01 17:56:51 +0900

    로그용 객체를 추가한다

---
+++ src/Logger.kt
@@ -0,0 +1,12 @@
+package src
+
+object Logger {
+
+    fun i( massage : string ){
+        println(" Log Info : $massage")
+    }
+
+    fun e( massage : string ){
+        println(" Log Error : $massage")
+    }
+}
